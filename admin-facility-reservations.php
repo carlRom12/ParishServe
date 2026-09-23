f3858decfd327df9d424f7d47355b310f1e477d0
jjay07 <ellanajayjay@gmail.com>
2026-09-23 16:36:16 +0800

    donation/massintention

--- admin-facility-reservations.php
+++
@@ -1,4 +0,0 @@
-<?php
-/** admin-facility-reservations.php -- Facility reservations; layout in includes/admin-request-page.php. */
-$requestType = 'facility';
-require __DIR__ . '/includes/admin-request-page.php';
